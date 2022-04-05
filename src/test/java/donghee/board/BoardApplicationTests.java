6d82cef88b321c894a4ad2378951a3001303a4fc
Jeong-DongHee <weg9511@gmail.com>
2022-04-05 14:10:50 +0900

    Create Project

---
+++ src/test/java/donghee/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package donghee.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
