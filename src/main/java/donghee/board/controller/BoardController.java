cecaf056ca6b7744c3d202eb3054bfb5af62beff
Jeong-DongHee <weg9511@gmail.com>
2022-04-07 23:35:42 +0900

    게시글 상세페이지 및 수정 컨트롤러 매핑

--- src/main/java/donghee/board/controller/BoardController.java
+++ src/main/java/donghee/board/controller/BoardController.java
@@ -1,17 +1,20 @@
 package donghee.board.controller;
 
 import donghee.board.domain.Post;
-import donghee.board.repository.BoardRepository;
 import donghee.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BoardController {
 
     final private BoardService boardService;
@@ -22,14 +25,44 @@ public class BoardController {
         return "main";
     }
 
-    @GetMapping("/create")
-    public String createForm() {
-        return "create";
+    @GetMapping("/post/add")
+    public String addForm() {
+        log.info("Add Form");
+
+        return "/post/addForm";
     }
 
-    @PostMapping("/create")
-    public String createPost(Post post){
-        boardService.create(post);
-        return "redirect:/";
+    @PostMapping("/post/add")
+    public String addPost(Post post, RedirectAttributes redirectAttributes) {
+        log.info("Add Post={}", post);
+        boardService.add(post);
+        redirectAttributes.addAttribute("postId", post.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/post/{postId}";
+    }
+
+    @GetMapping("/post/{postId}")
+    public String showPost(@PathVariable("postId") Long postId, Model model) {
+        Post post = boardService.findById(postId);
+        log.info("Show Post={}", post);
+        model.addAttribute("post", post);
+        return "/post/post";
+    }
+
+    @GetMapping("/post/{postId}/edit")
+    public String editForm(@PathVariable("postId") Long postId, Model model) {
+        Post post = boardService.findById(postId);
+        log.info("Edit Form");
+        model.addAttribute("post", post);
+        return "/post/editForm";
+    }
+
+    @PostMapping("/post/{postId}/edit")
+    public String editPost(@ModelAttribute Post editedPost, RedirectAttributes redirectAttributes) {
+        Post post = boardService.edit(editedPost);
+        log.info("Edit Post={}", post);
+        redirectAttributes.addAttribute("postId", post.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/post/{postId}";
     }
 }
